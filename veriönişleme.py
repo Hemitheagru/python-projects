8202153a3a79076430e3785a6dc30b5811fc6ca0
Hemitheagru <seydanuraydin50@gmail.com>
2024-12-31 15:28:13 +0300

    Update veriönişleme.py

--- veriönişleme.py
+++ veriönişleme.py
@@ -1,70 +1,70 @@
-# -*- coding: utf-8 -*-
-"""
-Spyder Editor
-
-This is a temporary script file.
-"""
-import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-##veriönisleme
-veriler=pd.read_csv('aylaragöresatis.csv')
+import pandas as pd
 
-veriler
-aylar=veriler[["Aylar"]]
-aylar
-satislar=veriler[["Satislar"]]
-satislar
-satislar2=veriler.iloc[:,0:1].values
-satislar2
+#kodlar
+#veri yukleme
 
+veriler = pd.read_csv('eksikveriler.csv')
+#pd.read_csv("veriler.csv")
 
-from sklearn.model_selection import train_test_split
-x_train,x_test,y_train,y_test=train_test_split(aylar,satislar,test_size=0.3,random_state=0)
+print(veriler)
 
-from sklearn.preprocessing import StandardScaler
-sc=StandardScaler()
-X_train=sc.fit_transform(x_train)
-X_test=sc.fit_transform(x_test)
+#veri on isleme
 
-Y_train=sc.fit_transform(y_train)
-Y_test=sc.fit_transform(y_test)
+boy = veriler[['boy']]
+print(boy)
 
-##model inşaası
-from sklearn.linear_model import LinearRegression
+boykilo = veriler[['boy','kilo']]
+print(boykilo)
 
-lr=LinearRegression()
-lr.fit(x_train,y_train)
-
-
-tahmin=lr.predict(x_test)
-x_train=x_train.sort_index()
-y_train=y_train.sort_index()
-plt.plot(x_train,y_train)
-plt.plot(x_test,lr.predict(x_train))
+x = 10
 
 
 
+#eksik veriler
+#sci - kit learn
 
+from sklearn.impute import SimpleImputer
 
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
 
+Yas = veriler.iloc[:,1:4].values
+print(Yas)
+imputer = imputer.fit(Yas[:,1:4])
+Yas[:,1:4] = imputer.transform(Yas[:,1:4])
+print(Yas)
 
+ulke = veriler.iloc[:,0:1].values
+print(ulke)
 
+from sklearn import preprocessing
 
+le = preprocessing.LabelEncoder()
 
+ulke[:,0] = le.fit_transform(veriler.iloc[:,0])
 
+print(ulke)
 
+ohe = preprocessing.OneHotEncoder()
+ulke = ohe.fit_transform(ulke).toarray()
+print(ulke)
 
+print(list(range(22)))
+sonuc = pd.DataFrame(data=ulke, index = range(22), columns = ['fr','tr','us'])
+print(sonuc)
 
+sonuc2 = pd.DataFrame(data=Yas, index = range(22), columns = ['boy','kilo','yas'])
+print(sonuc2)
 
+cinsiyet = veriler.iloc[:,-1].values
+print(cinsiyet)
 
+sonuc3 = pd.DataFrame(data = cinsiyet, index = range(22), columns = ['cinsiyet'])
+print(sonuc3)
 
+s=pd.concat([sonuc,sonuc2], axis=1)
+print(s)
 
-
-
-
-
-
-
-
-
+s2=pd.concat([s,sonuc3], axis=1)
+print(s2)
